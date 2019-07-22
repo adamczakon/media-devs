dfacf11ffd4905539def4f6921900b8dd30f5e95
Adam Czakon <adam.czakon63@gmail.com>
2019-07-22 11:44:43 +0200

    Updates

---
+++ precache-manifest.29d4dcc3f1aea04f31aabf74377129f5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2dd4f9c6cfee91b8637a84cd9097db",
+    "url": "/media-devs/index.html"
+  },
+  {
+    "revision": "a55b36d067781e7148e4",
+    "url": "/media-devs/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4ab4726138097ede3861",
+    "url": "/media-devs/static/js/2.087f29ad.chunk.js"
+  },
+  {
+    "revision": "a55b36d067781e7148e4",
+    "url": "/media-devs/static/js/main.1f9ebc2e.chunk.js"
+  },
+  {
+    "revision": "b0efb15967a9ef8aa57c",
+    "url": "/media-devs/static/js/runtime~main.e8a8a4e6.js"
+  },
+  {
+    "revision": "1426de104fc882b52d3bdaabef51ae9b",
+    "url": "/media-devs/static/media/client1.1426de10.jpg"
+  },
+  {
+    "revision": "f9f489704976ad2eb2a5f9f03899847d",
+    "url": "/media-devs/static/media/client2.f9f48970.jpg"
+  },
+  {
+    "revision": "cc13c929a29fb2cc52ee94141e42637e",
+    "url": "/media-devs/static/media/profile1.cc13c929.jpg"
+  },
+  {
+    "revision": "a4272d18f2041e2e53a7a62937de9c68",
+    "url": "/media-devs/static/media/profile2.a4272d18.jpg"
+  },
+  {
+    "revision": "b37ebc1835169f934f0b4c20ef45b75a",
+    "url": "/media-devs/static/media/profile3.b37ebc18.jpg"
+  },
+  {
+    "revision": "c92948e4c48a1a391d59fb206ed09c89",
+    "url": "/media-devs/static/media/showcase1.c92948e4.jpg"
+  },
+  {
+    "revision": "793ad8e8871be579395b185126a6f0ea",
+    "url": "/media-devs/static/media/showcase2.793ad8e8.jpg"
+  },
+  {
+    "revision": "471f5ad9e35afee07ab7fbdcaab3acfa",
+    "url": "/media-devs/static/media/showcase3.471f5ad9.jpg"
+  }
+]);
